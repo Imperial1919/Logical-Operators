0db9544cb30764e9233ecb7d0aaca57b10ace5d5
Bryce <ImperialEagle1998@gmail.com>
2020-09-23 13:47:00 -0500

    First Version of the Logical Operators Lesson Work.

---
+++ Logical_Operator_practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Logical_Operator_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool locomotiveissteam = false;
+            bool locomotiveissteam1 = true;
+
+            Console.WriteLine((locomotiveissteam1 && locomotiveissteam));
+            Console.WriteLine(locomotiveissteam1 = !locomotiveissteam);
+            Console.WriteLine(locomotiveissteam || locomotiveissteam1);
+            Console.WriteLine(locomotiveissteam1 || locomotiveissteam);
+            Console.WriteLine(locomotiveissteam1 || locomotiveissteam1);
+            Console.WriteLine(locomotiveissteam || locomotiveissteam);
+
+
+
+
+
+          
+            
+
+        
+        }
+    }
+}
